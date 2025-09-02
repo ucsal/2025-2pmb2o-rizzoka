dc62d9443920277ba9d61b18c9a74a9d59fa37fa
RizzoKa <kauarizzo06@gmail.com>
2025-09-02 17:38:22 -0300

    Classe DiscountCalculator - OCP ok

--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -1,12 +1,23 @@
 package br.com.mariojp.solid.ocp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DiscountCalculator {
-    public double apply(double amount, CustomerType type){
-        switch (type){
-            case REGULAR: return amount * 0.95;
-            case PREMIUM: return amount * 0.90;
-            default: return amount;
-            //teste 
-        }
-    }
+
+	Map<CustomerType, DiscountPolicy> politicas = new HashMap<>();
+
+	public DiscountCalculator(Map<CustomerType, DiscountPolicy> politicas) {
+		this.politicas = politicas;
+	}
+
+	public DiscountCalculator() {
+		this.politicas.put(CustomerType.REGULAR, new RegularPolicy());
+		this.politicas.put(CustomerType.PREMIUM, new PremiumPolicy());
+		this.politicas.put(CustomerType.PARTNER, new PartnerPolicy());
+	}
+
+	public double apply(double amount, CustomerType type) {
+		return politicas.get(type).apply(amount);
+	}
 }
